f8eab1989b397e4f3dab258bea7c589c0a6d3d72
stesev1 <32715588+stesev1@users.noreply.github.com>
2018-11-14 18:53:04 +0100

    Add files via upload

--- servers/__init__.py
+++ servers/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # StreamOnDemand Community Edition - Kodi Addon
-# ------------------------------------------------------------
+#------------------------------------------------------------
 # streamondemand - XBMC Plugin
 # http://www.mimediacenter.info/foro/viewforum.php?f=36
-# ------------------------------------------------------------
+#------------------------------------------------------------
 
 import os
 import sys
@@ -11,7 +11,7 @@ import sys
 # Appends the main plugin dir to the PYTHONPATH if an internal package cannot be imported.
 # Examples: In Plex Media Server all modules are under "Code.*" package, and in Enigma2 under "Plugins.Extensions.*"
 try:
-    # from platformcode import logger
+    #from platformcode import logger
     import core
 except:
-    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+    sys.path.append( os.path.abspath( os.path.join( os.path.dirname(__file__) , ".." ) ) )
---
+++ servers/fileflyer.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# StreamOnDemand Community Edition - Kodi Addon
+
+from core import scrapertools, logger
+
+
+def test_video_exists(page_url):
+    logger.info("(page_url='%s')" % page_url)
+
+    # Vídeo borrado: http://www.fileflyer.com/view/fioZRBu
+    # Video erróneo: 
+    data = scrapertools.cache_page(page_url)
+    if '<a href="/RemoveDetail.aspx">' in data:
+        return False,"Il video non è più disponibile<br/>su fileflyer.com (Video cancellato)"
+    else:
+        return True, ""
+
+
+def get_video_url(page_url, premium=False, user="", password="", video_password=""):
+    logger.info("(page_url='%s')" % page_url)
+    video_urls = []
+
+    data = scrapertools.cache_page(page_url)
+    location = scrapertools.get_match(data,
+                                      '<td class="dwnlbtn"[^<]+<a id="[^"]+" title="[^"]+" class="[^"]+" href="([^"]+)"')
+
+    video_urls.append([scrapertools.get_filename_from_url(location)[-4:] + " [fileflyer]", location])
+
+    for video_url in video_urls:
+        logger.info("%s - %s" % (video_url[0], video_url[1]))
+
+    return video_urls
