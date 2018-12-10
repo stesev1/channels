3b7c5d5c76e7f68dc66cbebcfcb48dc10de0b675
Dexter Morgan <9434916+32Dexter@users.noreply.github.com>
2018-12-10 21:42:06 +0100

    Add files via upload

--- channels/animesubita.py
+++ channels/animesubita.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-# Icarus - Kodi Addon
 # ------------------------------------------------------------
-# Icarus - XBMC Plugin
+# Thegroove360 - XBMC Plugin
 # Canale per AnimeSubIta
-# http://www.mimediacenter.info/foro/viewforum.php?f=36
 # ------------------------------------------------------------
 
 import re, urllib, urlparse
@@ -14,9 +12,7 @@ from core.item import Item
 from core.tmdb import infoSod
 
 __channel__ = "animesubita"
-
 host = "http://www.animesubita.org"
-
 headers = [['Referer', host]]
 
 # ----------------------------------------------------------------------------------------------------------------
@@ -26,27 +22,27 @@ def mainlist(item):
                      action="lista_anime_completa",
                      title=color("Lista Anime (%s)" % color("Slow", "red"), "azure"),
                      url="%s/lista-anime/" % host,
-                     thumbnail="http://orig03.deviantart.net/6889/f/2014/079/7/b/movies_and_popcorn_folder_icon_by_matheusgrilo-d7ay4tw.png"),
+                     thumbnail="https://raw.githubusercontent.com/stesev1/channels/master/images/channels_icon/popcorn_cinema_movie_.png"),
                 Item(channel=__channel__,
                      action="ultimiep",
                      title=color("Ultimi Episodi", "azure"),
                      url="%s/category/ultimi-episodi/" % host,
-                     thumbnail="http://orig03.deviantart.net/6889/f/2014/079/7/b/movies_and_popcorn_folder_icon_by_matheusgrilo-d7ay4tw.png"),
+                     thumbnail="https://raw.githubusercontent.com/stesev1/channels/master/images/channels_icon/popcorn_cinema_movie_.png"),
                 Item(channel=__channel__,
                      action="lista_anime",
                      title=color("Anime in corso", "azure"),
                      url="%s/category/anime-in-corso/" % host,
-                     thumbnail="http://orig03.deviantart.net/6889/f/2014/079/7/b/movies_and_popcorn_folder_icon_by_matheusgrilo-d7ay4tw.png"),
+                     thumbnail="https://raw.githubusercontent.com/stesev1/channels/master/images/channels_icon/popcorn_cinema_movie_.png"),
                 Item(channel=__channel__,
                      action="categorie",
                      title=color("Categorie", "azure"),
                      url="%s/generi/" % host,
-                     thumbnail="http://orig03.deviantart.net/6889/f/2014/079/7/b/movies_and_popcorn_folder_icon_by_matheusgrilo-d7ay4tw.png"),
+                     thumbnail="https://raw.githubusercontent.com/stesev1/channels/master/images/channels_icon/popcorn_cinema_movie_.png"),
                 Item(channel=__channel__,
                      action="search",
                      title=color("Cerca anime ...", "yellow"),
                      extra="anime",
-                     thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search")
+                     thumbnail="https://raw.githubusercontent.com/stesev1/channels/master/images/channels_icon/search_P.png")
                 ]
 
     return itemlist
@@ -193,7 +189,7 @@ def lista_anime(item, nextpage=True, show_lang=True):
                     action="lista_anime",
                     title=color("Successivo >>", "orange"),
                     url=scrapedurl,
-                    thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png",
+                    thumbnail="https://raw.githubusercontent.com/stesev1/channels/master/images/channels_icon/next_1.png",
                     folder=True))
 
     return itemlist
