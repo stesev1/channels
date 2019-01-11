f0ec5ba77364efeea1a726f8f562575b886a2108
Dexter Morgan <9434916+32Dexter@users.noreply.github.com>
2019-01-11 11:36:09 +0100

    Add files via upload

--- channels/altastreaming.py
+++ channels/altastreaming.py
@@ -1,158 +1,68 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------
 # TheGroove360 - XBMC Plugin
-# Canale altastreaming
+# Canale per Dexter Channel
 # ------------------------------------------------------------
 
 import re
-import urlparse
 
-from core import config, httptools
+from core import httptools
 from platformcode import logger
 from core import scrapertools
 from core import servertools
 from core.item import Item
-from core.tmdb import infoSod
 
 __channel__ = "altastreaming"
-
-host = "http://altadefinizione.press"
-
-headers = [['Referer', host]]
 
 
 def mainlist(item):
     logger.info("[thegroove360.altastreaming] mainlist")
+
     itemlist = [Item(channel=__channel__,
-                     title="[COLOR azure]Aggiornamenti Film[/COLOR]",
-                     action="peliculas",
-                     url="%s/nuove-uscite/" % host,
-                     extra="movie",
-                     thumbnail=""),
-                Item(channel=__channel__,
-                     title="[COLOR azure]Film Per Categoria[/COLOR]",
-                     action="categorias",
-                     url="%s/nuove-uscite/" % host,
-                     thumbnail=""),
-                Item(channel=__channel__,
-                     title="[COLOR yellow]Cerca...[/COLOR]",
-                     action="search",
-                     extra="movie",
-                     thumbnail="")]
+                     title="[COLOR azure]Dexter Channel - [/COLOR][COLOR orange]I Film[/COLOR]",
+                     action="lista",
+                     url="https://raw.githubusercontent.com/32Dexter/DexterRepo/master/Dexter%20Channel/Dexter%20Channel.txt",
+                     thumbnail="https://raw.githubusercontent.com/stesev1/channels/master/images/channels_icon/popcorn_cinema_movie_.png",
+                     fanart="https://raw.githubusercontent.com/32Dexter/DexterRepo/master/Dexter%20Channel/dexter_wallpaper.jpg")]
 
     return itemlist
 
 
-def categorias(item):
+def lista(item):
+    logger.info("[thegroove360.altastreaming] lista")
+
     itemlist = []
 
-    # Carica la pagina 
-    data = httptools.downloadpage(item.url, headers=headers).data
-    bloque = scrapertools.get_match(data, '<ul class="listSubCat" id="Film">(.*?)</ul>')
+    # Scarica la pagina
+    data = httptools.downloadpage(item.url).data
 
-    # Estrae i contenuti 
-    patron = '<li><a href="([^"]+)">(.*?)</a></li>'
-    matches = re.compile(patron, re.DOTALL).findall(bloque)
-
-    for scrapedurl, scrapedtitle in matches:
-        scrapedurl = host + scrapedurl
-        itemlist.append(
-            Item(channel=__channel__,
-                 action="peliculas",
-                 title="[COLOR azure]" + scrapedtitle + "[/COLOR]",
-                 url=scrapedurl,
-                 thumbnail="",
-                 folder=True))
-
-    return itemlist
-
-
-def search(item, texto):
-    logger.info("[thegroove360.altastreaming] " + item.url + " search " + texto)
-    item.url = host + "/?do=search&subaction=search&story=" + texto
-    try:
-        if item.extra == "movie":
-            return peliculas(item)
-    # Continua la ricerca in caso di errore 
-    except:
-        import sys
-        for line in sys.exc_info():
-            logger.error("%s" % line)
-        return []
-
-
-def newest(categoria):
-    logger.info("[thegroove360.altastreaming] newest" + categoria)
-    itemlist = []
-    item = Item()
-    try:
-        if categoria == "peliculas":
-            item.url = host
-            item.action = "peliculas"
-            itemlist = peliculas(item)
-
-            if itemlist[-1].action == "peliculas":
-                itemlist.pop()
-
-    # Continua la ricerca in caso di errore 
-    except:
-        import sys
-        for line in sys.exc_info():
-            logger.error("{0}".format(line))
-        return []
-
-    return itemlist
-
-def peliculas(item):
-    logger.info("[thegroove360.altastreaming] peliculas")
-    itemlist = []
-
-    # Carica la pagina 
-    data = httptools.downloadpage(item.url, headers=headers).data
-
-    # Estrae i contenuti 
-    patron = '<h2 class="titleFilm"><a href="([^"]+)">([^<]+)<\/a><\/h2>'
+    # Estrae i contenuti
+    patron = '<a href="(.*?)".*<h1>(.*?)<.*?"(.*?)"'
     matches = re.compile(patron, re.DOTALL).findall(data)
 
-    for scrapedurl, scrapedtitle in matches:
-        scrapedplot = ""
-        scrapedthumbnail = ""
+    for scrapedurl, scrapedtitle, scrapedthumbnail in matches:
         scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle)
-
-        itemlist.append(infoSod(
+        itemlist.append(
             Item(channel=__channel__,
                  action="findvideos",
-                 contentType="movie",
-                 fulltitle=scrapedtitle,
-                 show=scrapedtitle,
-                 title="[COLOR azure]" + scrapedtitle + "[/COLOR]",
-                 url=scrapedurl,
+                 title=scrapedtitle,
                  thumbnail=scrapedthumbnail,
-                 plot=scrapedplot,
-                 folder=True), tipo='movie'))
-
-    # Paginazione 
-    patronvideos = '<a href="([^"]+)">Succ'
-    matches = re.compile(patronvideos, re.DOTALL).findall(data)
-
-    if len(matches) > 0:
-        scrapedurl = urlparse.urljoin(item.url, matches[0])
-        itemlist.append(
-            Item(channel=__channel__,
-                 action="HomePage",
-                 title="[COLOR yellow]Torna Home[/COLOR]",
-                 folder=True)),
-        itemlist.append(
-            Item(channel=__channel__,
-                 action="peliculas",
-                 title="[COLOR orange]Successivo >>[/COLOR]",
-                 url=scrapedurl,
-                 thumbnail="",
-                 folder=True))
+                 url=scrapedurl))
 
     return itemlist
 
-def HomePage(item):
-    import xbmc
-    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.Stefano)")
 
+def findvideos(item):
+    logger.info("[thegroove360.altastreaming] findvideos")
+
+    # Downloads page
+    data = item.url
+
+    itemlist = servertools.find_video_items(data=data)
+    for videoitem in itemlist:
+        videoitem.title = item.title + "[COLOR orange]" + videoitem.title + "[/COLOR]"
+        videoitem.fulltitle = item.fulltitle
+        videoitem.thumbnail = item.thumbnail
+        videoitem.channel = __channel__
+
+    return itemlist
