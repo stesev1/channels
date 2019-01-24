75d68f8576b186290949a05a073b1426f4fa9fe9
Dexter Morgan <9434916+32Dexter@users.noreply.github.com>
2019-01-24 13:06:28 +0100

    Add files via upload

--- channels/ilgeniodellostreaming.py
+++ channels/ilgeniodellostreaming.py
@@ -17,7 +17,7 @@ from core.tmdb import infoSod
 from core import cloudflare
 
 __channel__ = "ilgeniodellostreaming"
-host = "https://ilgeniodellostreaming.red"
+host = "https://ilgeniodellostreaming.app"
 headers = [
     ['User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100101 Firefox/44.0'],
     ['Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'],
