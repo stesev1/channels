60d03a64aa35b6eed8d7e522cf6e0fd9e7aa8602
Dexter Morgan <9434916+32Dexter@users.noreply.github.com>
2019-01-28 09:49:27 +0100

    Add files via upload

--- channels/filmzstreaming.py
+++ channels/filmzstreaming.py
@@ -1,1 +1,195 @@
-404: Not Found
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------
+# TheGroove360 - XBMC Plugin
+# Canale filmzstreaming
+# ------------------------------------------------------------
+
+import re
+import urlparse
+
+from core import httptools
+from platformcode import logger
+from core import scrapertools
+from core import servertools
+from core.item import Item
+from core.tmdb import infoSod
+
+__channel__ = "filmzstreaming"
+
+host = "https://filmzstreaming.pw"
+
+headers = [['Referer', host]]
+
+def mainlist(item):
+    logger.info("[thegroove360.filmzstreaming] mainlist")
+
+    itemlist = [Item(channel=__channel__,
+                     title="[COLOR azure]Ultimi film inseriti[/COLOR]",
+                     action="peliculas",
+                     url="%s/film/" % host,
+                     thumbnail="https://raw.githubusercontent.com/stesev1/channels/master/images/channels_icon/movie_new_P.png"),
+                Item(channel=__channel__,
+                     title="[COLOR azure]Categorie film[/COLOR]",
+                     action="categorias",
+                     url=host,
+                     thumbnail="https://raw.githubusercontent.com/stesev1/channels/master/images/channels_icon/movie_new_P.png"),
+                Item(channel=__channel__,
+                     title="[COLOR yellow]Cerca...[/COLOR]",
+                     action="search",
+                     extra="movie",
+                     thumbnail="https://raw.githubusercontent.com/stesev1/channels/master/images/channels_icon/search_P.png")]
+
+    return itemlist
+
+def peliculas(item):
+    logger.info("[thegroove360.filmzstreaming] peliculas")
+    itemlist = []
+
+    # Carica la pagina 
+    data = httptools.downloadpage(item.url, headers=headers).data
+
+    # Estrae i contenuti 
+    patron = '<span class="flag"[^>]+><\/span>\s*<a href="([^"]+)">(.*?)<'
+    matches = re.compile(patron, re.DOTALL).findall(data)
+
+    for scrapedurl, scrapedtitle in matches:
+        scrapedplot = ""
+        scrapedthumbnail = ""
+        scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle)
+        scrapedtitle = scrapedtitle.replace("Streaming ", "")
+
+        itemlist.append(infoSod(
+            Item(channel=__channel__,
+                 action="findvideos",
+                 contentType="movie",
+                 title=scrapedtitle,
+                 fulltitle=scrapedtitle,
+                 url=scrapedurl,
+                 thumbnail=scrapedthumbnail), tipo="movie"))
+
+    # Paginazione 
+    patronvideos = '<span class="current">[^>]+</span><a href=\'(.*?)\' class="inactive">'
+    matches = re.compile(patronvideos, re.DOTALL).findall(data)
+
+    if len(matches) > 0:
+        scrapedurl = urlparse.urljoin(item.url, matches[0])
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="HomePage",
+                 title="[COLOR yellow]Torna Home[/COLOR]",
+                 folder=True)),
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="peliculas",
+                 title="[COLOR orange]Successivo >>[/COLOR]",
+                 url=scrapedurl,
+                 thumbnail="https://raw.githubusercontent.com/stesev1/channels/master/images/channels_icon/next_1.png",
+                 folder=True))
+
+    return itemlist
+
+def peliculas_src(item):
+    logger.info("[thegroove360.filmzstreaming] peliculas")
+    itemlist = []
+
+    # Carica la pagina 
+    data = httptools.downloadpage(item.url, headers=headers).data
+
+    # Estrae i contenuti 
+    patron = '<div class="title">\s*<a href="([^"]+)">(.*?)</a>'
+    matches = re.compile(patron, re.DOTALL).findall(data)
+
+    for scrapedurl, scrapedtitle in matches:
+        scrapedplot = ""
+        scrapedthumbnail = ""
+        scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle)
+        scrapedtitle = scrapedtitle.replace("Streaming ", "")
+
+        itemlist.append(infoSod(
+            Item(channel=__channel__,
+                 action="findvideos",
+                 contentType="movie",
+                 title=scrapedtitle,
+                 fulltitle=scrapedtitle,
+                 url=scrapedurl,
+                 thumbnail=scrapedthumbnail), tipo="movie"))
+
+    # Paginazione 
+    patronvideos = '<span class="current">[^>]+</span><a href=\'(.*?)\' class="inactive">'
+    matches = re.compile(patronvideos, re.DOTALL).findall(data)
+
+    if len(matches) > 0:
+        scrapedurl = urlparse.urljoin(item.url, matches[0])
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="HomePage",
+                 title="[COLOR yellow]Torna Home[/COLOR]",
+                 folder=True)),
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="peliculas_src",
+                 title="[COLOR orange]Successivo >>[/COLOR]",
+                 url=scrapedurl,
+                 thumbnail="https://raw.githubusercontent.com/stesev1/channels/master/images/channels_icon/next_1.png",
+                 folder=True))
+
+    return itemlist
+
+def HomePage(item):
+    import xbmc
+    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.stefano/)")
+
+
+def categorias(item):
+    logger.info("[thegroove360.filmzstreaming] categorias")
+    itemlist = []
+
+    data = httptools.downloadpage(item.url, headers=headers).data
+
+    # Narrow search by selecting only the combo
+    bloque = scrapertools.get_match(data, '<ul class="sub-menu">(.*?)</ul>')
+
+    # The categories are the options for the combo
+    patron = '<a href="([^"]+)">(.*?)</a></li>'
+    matches = re.compile(patron, re.DOTALL).findall(bloque)
+
+    for scrapedurl, scrapedtitle in matches:
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="peliculas",
+                 title="[COLOR azure]" + scrapedtitle + "[/COLOR]",
+                 url=scrapedurl,
+                 thumbnail="https://raw.githubusercontent.com/stesev1/channels/master/images/channels_icon/movie_new_P.png",
+                 folder=True))
+
+    return itemlist
+
+def search(item, texto):
+    logger.info("[thegroove360.filmzstreaming] " + item.url + " search " + texto)
+    item.url = host + "/?s=" + texto
+    try:
+        return peliculas_src(item)
+    # Continua la ricerca in caso di errore 
+    except:
+        import sys
+        for line in sys.exc_info():
+            logger.error("%s" % line)
+        return []
+
+def findvideos(item):
+    logger.info("[thegroove360.filmzstreaming] findvideos")
+
+    # Carica la pagina 
+    data = httptools.downloadpage(item.url, headers=headers).data
+
+    itemlist = servertools.find_video_items(data=data)
+
+    for videoitem in itemlist:
+        videoitem.title = "".join([item.title, '[COLOR green][B]' + videoitem.title + '[/B][/COLOR]'])
+        videoitem.fulltitle = item.fulltitle
+        videoitem.show = item.show
+        videoitem.thumbnail = item.thumbnail
+        videoitem.channel = __channel__
+
+    return itemlist
+
