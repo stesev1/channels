de628eef1ed82ce66f315832c817d1b4e40c39a8
Dexter Morgan <9434916+32Dexter@users.noreply.github.com>
2019-03-21 16:48:52 +0100

    Add files via upload

--- servers/speedvideo.py
+++ servers/speedvideo.py
@@ -29,34 +29,12 @@ def get_video_url(page_url,
 
     data = httptools.downloadpage(page_url).data
 
-    media_url = scrapertools.find_single_match(data, 'linkfile ="([^"]+)"')
+    media_urls = scrapertools.find_multiple_matches(data, r"file:[^']'([^']+)',\s*label:[^\"]\"([^\"]+)\"")
 
-    media_url = scrapertools.getLocationHeaderFromResponse(media_url)
+    for media_url, label in media_urls:
+        media_url = httptools.downloadpage(media_url, only_headers=True, follow_redirects=False).headers.get("location", "")
 
-    video_urls.append(
-        ["." + media_url.rsplit('.', 1)[1] + ' [speedvideo]', media_url])
+        if media_url:
+            video_urls.append([label + " " + media_url.rsplit('.', 1)[1] + ' [speedvideo]', media_url])
 
     return video_urls
-
-# Encuentra videos de este servidor en el texto pasado
-def find_videos(text):
-    encontrados = set()
-    devuelve = []
-
-    # http://speedvideo.net/embed-fmbvopi1381q-530x302.html
-    # http://speedvideo.net/hs7djap7jwrw/Tekken.Kazuyas.Revenge.2014.iTALiAN.Subbed.DVDRiP.XViD.NeWZoNe.avi.html
-    patronvideos = 'speedvideo.net/(?:embed-|)([A-Z0-9a-z]{12})'
-    logger.info("#" + patronvideos + "#")
-    matches = re.compile(patronvideos, re.DOTALL).findall(text)
-
-    for match in matches:
-        titulo = "[speedvideo]"
-        url = "http://speedvideo.net/embed-%s.html" % match
-        if url not in encontrados and url != "http://speedvideo.net/embed":
-            logger.info("  url=" + url)
-            devuelve.append([titulo, url, 'speedvideo'])
-            encontrados.add(url)
-        else:
-            logger.info("  url duplicada=" + url)
-
-    return devuelve
